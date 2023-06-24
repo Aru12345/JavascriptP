a9a0f9b272e64df09ff46725d70af1c1da0e557b
Arundhati <arundhatiraicar12@gmail.com>
2023-06-24 18:42:17 -0400

    Current Position

---
+++ READ.md
@@ -0,0 +1,11 @@
+# New Project
+
+# Requirements
+
+- Have json-servor installed
+
+### Installation Instructions
+
+- Fork and clone
+- Run json server for db.json
+- Open index.html in the browser
---
+++ javascripts/index.js
@@ -0,0 +1,11 @@
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
